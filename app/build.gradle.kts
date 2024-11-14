b1af10a0b241caaae5c82a159ce0dc8c722a78e0
roisin-m <s00223865@atu.ie>
2024-11-14 16:15:22 +0000

    setup z library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    implementation("com.google.zxing:core:3.4.1")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
